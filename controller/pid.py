7dbe00f7103d08a2fabdd40d491c95a4cb53d152
dogtreatfairy <ryansteel87@gmail.com>
2024-11-06 11:10:36 -0600

    Updates to pid

--- controller/pid.py
+++ controller/pid.py
@@ -63,6 +63,8 @@ class Controller(ControllerBase):
 		self.last = 150
 
 		self.set_target(0.0)
+		
+		self.new_setpoint_up = False
 
 	def _calculate_gains(self, pb, ti, td):
 		self.kp = -1 / pb
@@ -89,11 +91,18 @@ class Controller(ControllerBase):
 
 		# PID
 		self.u = self.p + self.i + self.d
+		
+		rate_of_change = abs(self.u - self.last) / dt
+		if rate_of_change > 1 and current < self.controller.set_point - 10:
+			self.u = self.u/2
 
 		# Update for next cycle
 		self.error = error
 		self.last = current
 		self.last_update = time.time()
+		
+		if self.new_setpoint_up and current >= self.set_point:
+			self.new_setpoint_up = False
 
 		return self.u
 
@@ -103,7 +112,9 @@ class Controller(ControllerBase):
 		self.inter = 0.0
 		self.derv = 0.0
 		self.last_update = time.time()
-
+		if self.last < self.set_point:
+			self.new_setpoint_up = True
+		
 	def set_gains(self, pb, ti, td):
 		self._calculate_gains(pb,ti,td)
 		self.inter_max = abs(self.center / self.ki)
