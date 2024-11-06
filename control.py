71eb976c7370cc1e70c4e7add7f059abf726d18a
dogtreatfairy <ryansteel87@gmail.com>
2024-11-06 14:12:48 -0600

    h

--- control.py
+++ control.py
@@ -624,14 +624,15 @@ def _work_cycle(mode, grill_platform, probe_complex, display_device, dist_device
 			# Update last temperature and time
 			if last_set_point < control['primary_setpoint']:
 				new_set_point = True
+				last_set_point = control['primary_setpoint']
 			
-			if last_set_point <= ptemp <= last_set_point + 3:
+			if last_set_point == ptemp <= last_set_point + 3:
 				new_set_point = False
 				last_set_point = control['primary_setpoint']
 
 			if ptemp > control['primary_setpoint'] + 10:
 				overshoot = True
-				controllerCore.set_target(control['primary_setpoint'])
+				controllerCore.set_target(last_set_point)
 		
 			# If Auger is ON and Overshoot is Detected
 			if mode == 'Hold' and new_set_point and ptemp >= control['primary_setpoint'] and (now - auger_toggle_time) > (CycleTime * settings['cycle_data']['u_min']):
