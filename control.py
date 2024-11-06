38dfd335d097fe88796da8da83bed6cb51de7303
dogtreatfairy <ryansteel87@gmail.com>
2024-11-06 12:11:29 -0600

    up

--- control.py
+++ control.py
@@ -617,6 +617,7 @@ def _work_cycle(mode, grill_platform, probe_complex, display_device, dist_device
 		# Change Auger State based on Cycle Time
 		if mode in ('Startup', 'Reignite', 'Smoke', 'Hold', 'Prime'):
 			# If Auger is OFF and time since toggle is greater than Off Time
+			new_setpoint_up = False
 			new_setpoint_up = controllerCore.get_new_setpoint_up
 			if not current_output_status['auger'] and (now - auger_toggle_time) > (CycleTime * (1 - CycleRatio)):
 				grill_platform.auger_on()
