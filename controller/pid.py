8f0729a2cb1318f91b24fcfed6eb91fe763721d1
dogtreatfairy <ryansteel87@gmail.com>
2024-11-08 06:23:39 -0600

    Derate multiplier logic

--- controller/pid.py
+++ controller/pid.py
@@ -60,9 +60,11 @@ class Controller(ControllerBase):
 		self.rate_of_change = 0.0
 		
 		self.derate_window = config['derate_window']
-		self.derate_multiplier = config['derate_multiplier']
+		self.user_derate_multiplier = config['derate_multiplier']
+		self.derate_multiplier = self.user_derate_multiplier
 		self.derate = False
 		self.derate_start_time = None
+		self.rerate_increment = 0.1
 		
 		self.stable_time = config['stable_time']
 		self.stable_window = config['stable_window']
@@ -109,10 +111,22 @@ class Controller(ControllerBase):
 		if self.new_target and abs(error) <= self.derate_window and rate_of_change >= self.max_rate_of_change:
 			self.derate = True
 			self.derate_start_time = time.time()
-		
+
 		# If derate is true, derate the output by the derate multiplier
 		if self.derate:
 			self.u = self.u * self.derate_multiplier
+		
+			# Gradually increase the derate multiplier until it reaches 1
+			if self.derate_multiplier < 1:
+				self.derate_multiplier += self.rerate_increment
+				self.derate_multiplier = min(self.derate_multiplier, 1)  # Ensure it does not exceed 1
+		
+			# If derated longer than the stable window, reset derate flag
+			if self.derate_start_time is not None and self.stable_time is not None and self.derate_multiplier == 1:
+				if (time.time() - self.derate_start_time) >= self.stable_time:
+					self.derate = False
+					self.derate_start_time = None
+					self.derate_multiplier = self.user_derate_multiplier
 		
 		# If derated longer than the stable window, reset derate flag
 		if not self.derate_start_time == None and not self.stable_time == None:
@@ -129,7 +143,6 @@ class Controller(ControllerBase):
 				self.within_range_start = time.time()
 			elif time.time() - self.within_range_start >= self.stable_time:
 				self.new_target = False
-				self.derate = False
 				self.error = 0.0
 				self. inter = 0.0
 				self.derv = 0.0
