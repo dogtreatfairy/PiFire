366dd584a3c5863af5f5868a8c23d040bd0b20e0
dogtreatfairy <ryansteel87@gmail.com>
2024-11-10 16:53:26 -0600

    derate for low set_points

--- controller/pid.py
+++ controller/pid.py
@@ -78,7 +78,8 @@ class Controller(ControllerBase):
 
 		self.last = 150
 		self.start_change_temp = 0.0
-
+		self.low_set_point = False
+		self.low_set_point_counter = 0
 		self.set_target(0.0)
 		self.new_target = False
 		self.within_range_start = None
@@ -123,6 +124,15 @@ class Controller(ControllerBase):
 		self.eventLogger.info(f"I:  {self.i}")
 		self.eventLogger.info(f"D:  {self.d}")
 		self.eventLogger.info(f"U:  {self.u}")
+
+		# Derate 3 cycles for low set_points
+		if self.low_set_point:
+			self.derate = True
+			self.derate_multiplier = 0.7
+			self.low_set_point_counter += 1
+			if self.low_set_point_counter >= 3:
+				self.low_set_point = False
+				self.low_set_point_counter = 0
 
 		# If rate of change is too high within derate window during a set point change, derate output
 		if self.new_target and ((abs(error) <= self.derate_window) or (100 < self.set_point < 225 and abs(error) <= self.derate_window + 10)) and self.derv >= self.center and error < 0 and not self.derate:
@@ -191,6 +201,8 @@ class Controller(ControllerBase):
 		self.start_change_temp = self.last
 		self.new_target = True
 		self.derate = False
+		if set_point < 200:
+			self.low_set_point = True
 		self.eventLogger.info(f"New Set Point: {self.set_point}")
     
 	def set_gains(self, pb, ti, td):
