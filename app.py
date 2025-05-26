19a70c2e4b73f50110af307f94298c5804663d60
dogtreatfairy <98328354+dogtreatfairy@users.noreply.github.com>
2025-05-26 12:52:49 -0500

    import read_probe_status

--- app.py
+++ app.py
@@ -27,11 +27,12 @@ import zipfile
 import pathlib
 from datetime import datetime
 from updater import *  # Library for doing project updates from GitHub
-from file_mgmt.common import fixup_assets, read_json_file_data, update_json_file_data, remove_assets, read_probe_status
+from file_mgmt.common import fixup_assets, read_json_file_data, update_json_file_data, remove_assets
 from file_mgmt.cookfile import read_cookfile, upgrade_cookfile, prepare_chartdata
 from file_mgmt.media import add_asset, set_thumbnail, unpack_thumb
 from file_mgmt.recipes import read_recipefile, create_recipefile
-from common.socketio_handler import socketio # Moved SocketIO to a separate file in common
+from common.common import *
+from common.socketio_handler import socketio
 import json
 import os
 
